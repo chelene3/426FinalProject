be1c73d7473e87d0ff718318e2d58eb9628c80ae
Claire Su <suxiao10claire@gmail.com>
2020-11-13 12:25:28 -0500

    working backend ?

--- render_pages.js
+++ render_pages.js
@@ -13,3 +13,23 @@
 //       map: map,
 //     });
 //   }
+
+const location1 = async  () =>{
+    try{
+        const res = await axios({
+            method: 'get',
+            url: 'http://localhost:3000/location/1',
+        })
+        console.log(res.data);
+      // $('#test').append(`<p>${res.data.name}</p>`);
+
+    }catch(err){
+        console.error(err);
+    }
+}
+location1();
+/*
+ url for post: 'http://localhost:3000/location'
+ url for put (update) : 'http://localhost:3000/location/{id}'
+ url for delete: 'http://localhost:3000/location/{id}'
+*/
