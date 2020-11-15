b3a6274926fa838c52637ceac9ede141308d9df1
Chelsea Rowe <chelene@live.unc.edu>
2020-11-15 14:25:34 -0500

    connects to specific pages from search bar

--- home.js
+++ home.js
@@ -41,6 +41,28 @@ $ (function () {
   });
 });
 
+//TRY TO GET THIS FROM BACKEND NOT HARDCODED
+let dictVals = {
+    "Starbucks": 1,
+    "Davis Library": 2,
+    "Robert B. House Undergraduate Library": 3,
+    "Stone and Leaf Cafe": 4,
+    "Fedex Center": 5,
+    "Health Science Library": 6,
+    "Stone Library": 7,
+    "Chapel Hill Public Library": 8,
+    "Summit Franklin": 9,
+    "Honeysuckle Cafe and Bar": 10,
+    "Coco Bean Coffee Shop": 11,
+    "Johnny's Gone Fishing": 12,
+    "Cafe Driade": 13,
+    "Epilogue Books Chocolate Brews": 14,
+    "Wilson Library": 15,
+    "Carolina Union": 16,
+    "Cha House": 17,
+    "Carolina Coffee Shop": 18
+}
+
 const registerSearchButton = function () {
   document.querySelectorAll("#homesearch").forEach(item => {
       item.addEventListener("click", handleSearchButtonPress);
@@ -48,8 +70,7 @@ const registerSearchButton = function () {
 }
 
 const handleSearchButtonPress = function (event) {
-  window.location.href="mock_location_page.html";
-  //TO DO LINK TO SPECIFIC PAGE RATHER THAN JUST MOCK LOCATION PAGE
+  window.location.href="mock_location_page.html?name=" + dictVals[document.getElementById('searchText').value];
 };
 
 // https://www.geeksforgeeks.org/login-form-using-node-js-and-mongodb/
