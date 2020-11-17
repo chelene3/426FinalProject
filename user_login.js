4e7068ffcd10efe565e3c5e8e89d47e8c2754fb6
Eliana Wilkoff <ewilkoff@Elianas-MacBook-Air-2.local>
2020-11-16 19:51:59 -0500

    login create user button

--- user_login.js
+++ user_login.js
@@ -12,4 +12,25 @@ $(function () {
             }
         })
 })
+
+$(document).on('click', '#createaccount-submit', async function(event) {
+    event.preventDefault();
+    let userTyped = $("#user-new-input").val();
+    let password1 = $("#password-one").val();
+    let password2= $("#password-two").val();
+    
+    if (password1 != password2) {
+        alert("Passwords do not match, try again!");
+        return;
+    } else {
+        const result = await axios( {
+            method: 'post',
+            url: 'http://localhost:3000/login',
+            data: {
+                username: `${userTyped}`,
+                password: `${password1}`
+            }
+        })
+    }
+})
 })
