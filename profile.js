032dbb54a5bde7de3085cc4f028df94235b86bf1
Chelsea Rowe <chelene@live.unc.edu>
2020-11-20 11:44:49 -0600

    username shows up

--- profile.js
+++ profile.js
@@ -4,38 +4,38 @@
 // var router = express.Router();
 
 // import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-var express = require('express');
-const app = express();
-var router = express.Router();
+// const require = createRequire(import.meta.url);
+// var express = require('express');
+// const app = express();
+// var router = express.Router();
 
 $(function () {
 
     // getAllIDsForUser(session.username);
     // getUserInfo();
 });   
-var session;
-app.get('/', function(req, res, next) {
-    session = req.session;
-    console.log("user is: " + session.username);
-})
+// var session;
+// app.get('/', function(req, res, next) {
+//     session = req.session;
+//     console.log("user is: " + session.username);
+// })
 
 // axios.defaults.withCredentials = true;
 async function getUserInfo(){
     const result = await axios( {
         method: 'get',
-        url: 'https://enigmatic-meadow-24377.herokuapp.com/user',
+        url: 'http://localhost:3003/user',
         withCredentials: true
 
     });
     console.log(result.data);
-    $('#username').append(result.data.username);
+    $('#username').append(`<p>${result.data.username}</p>`);
 
 }
 async function getSecrets(){
     const result = await axios( {
         method: 'get',
-        url: 'https://enigmatic-meadow-24377.herokuapp.com/secret',
+        url: 'http://localhost:3003/secret',
         withCredentials: true
 
     });
