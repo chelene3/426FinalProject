11af414f117864fd171f81f3035bd8ce651b88d3
Eliana Wilkoff <ewilkoff@Elianas-MacBook-Air-2.local>
2020-11-20 11:27:50 -0500

    appending secret objects

--- profile.js
+++ profile.js
@@ -4,30 +4,51 @@
 // var router = express.Router();
 
 // import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-var express = require('express');
-const app = express();
-var router = express.Router();
+// const require = createRequire(import.meta.url);
+// var express = require('express');
+// const app = express();
+// var router = express.Router();
 
 $(function () {
 
     // getAllIDsForUser(session.username);
-    // getUserInfo();
+   
+    getUserInfo();
 });   
-var session;
-app.get('/', function(req, res, next) {
-    session = req.session;
-    console.log("user is: " + session.username);
-})
+// var session;
+// app.get('/', function(req, res, next) {
+//     session = req.session;
+//     console.log("user is: " + session.username);
+// })
 
 // axios.defaults.withCredentials = true;
 async function getUserInfo(){
     const result = await axios( {
         method: 'get',
-        url: 'http://localhost:3000/secret',
-        withCredentials: false
+        url: 'http://localhost:3003/secret',
+        withCredentials: true
 
     });
-    console.log(result.data);
+    for(let i = 0; i < result.data.length; i++) {
+        getInfoIds(result.data[i]);
+        
+    }    
+}
+
+
+async function getInfoIds(id) {
+    const result = await axios( {
+        method: 'get',
+        url: `http://localhost:3003/secret/${id}`,
+        withCredentials: true
+    })
+    currentUser = result.data.username;
+    let htmlTemp = `<div class="box"><p>${result.data.secret}</p></div>`;
+    $("#likedPostsDiv").append(htmlTemp);
+
+    let nameTemp = `<h2><b>${result.data.username}</b></h2>`;
+    $("#usernameHeader").replaceWith(nameTemp);
 
 }
+
+
