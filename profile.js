0766ce040d3806b7a7a7815c345f3438918207bc
Chelsea Rowe <chelene@live.unc.edu>
2020-11-20 11:12:58 -0600

    fixed profile.js

--- profile.js
+++ profile.js
@@ -1,6 +1,24 @@
+// var express = require('express');
+// const app = express();
+// const port = 3000;
+// var router = express.Router();
+
+// import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+var express = require('express');
+const app = express();
+var router = express.Router();
+
 $(function () {
- 
+
+    // getAllIDsForUser(session.username);
+    // getUserInfo();
 });   
+var session;
+app.get('/', function(req, res, next) {
+    session = req.session;
+    console.log("user is: " + session.username);
+})
 
 // axios.defaults.withCredentials = true;
 async function getUserInfo(){
@@ -14,7 +32,6 @@ async function getUserInfo(){
     $('#username').append(result.data.username);
 
 }
- 
 async function getSecrets(){
     const result = await axios( {
         method: 'get',
