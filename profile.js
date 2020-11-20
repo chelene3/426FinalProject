b1d6630955bac076e7dc995a361aa4dd9909877e
Eliana Wilkoff <ewilkoff@Elianas-MacBook-Air-2.local>
2020-11-20 10:57:11 -0500

    issues with profile page connection

--- profile.js
+++ profile.js
@@ -1,8 +1,24 @@
-const expressSession = require('express-session');
+// var express = require('express');
+// const app = express();
+// const port = 3000;
+// var router = express.Router();
+
+// import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+var express = require('express');
+const app = express();
+var router = express.Router();
 
 $(function () {
- 
+
+    // getAllIDsForUser(session.username);
+    // getUserInfo();
 });   
+var session;
+app.get('/', function(req, res, next) {
+    session = req.session;
+    console.log("user is: " + session.username);
+})
 
 // axios.defaults.withCredentials = true;
 async function getUserInfo(){
@@ -15,6 +31,3 @@ async function getUserInfo(){
     console.log(result.data);
 
 }
- 
-alert(session.user);
-getUserInfo();
